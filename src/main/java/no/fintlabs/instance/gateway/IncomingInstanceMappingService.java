9a22d993a9ef4a2b5c0137b5ca2ed3b18108a7c7
Nils-Odd Solberg <nilsodd@fintlabs.no>
2024-04-19 10:08:16 +0200

    ARKIV-259 The next step towards the Troms thing.

--- src/main/java/no/fintlabs/instance/gateway/IncomingInstanceMappingService.java
+++ src/main/java/no/fintlabs/instance/gateway/IncomingInstanceMappingService.java
@@ -10,7 +10,9 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -36,35 +38,37 @@ public class IncomingInstanceMappingService implements InstanceMapper<IncomingIn
     }
 
     private static Map<String, String> toValuePerKey(IncomingInstance incomingInstance, UUID uuid) {
+        Set<Map.Entry<String, String>> entries = new HashSet<>();
 
-        assert incomingInstance.getTilleggsinformasjon() != null;
-        return Stream.of(
-                Map.entry("personaliaFodselsnummer", incomingInstance.getPersonalia().getFodselsnummer()),
-                Map.entry("personaliaFornavn", incomingInstance.getPersonalia().getFornavn()),
-                Map.entry("personaliaMellomnavn", incomingInstance.getPersonalia().getMellomnavn()),
-                Map.entry("personaliaEtternavn", incomingInstance.getPersonalia().getEtternavn()),
 
-                Map.entry("kontaktinformasjonTelefonnummer", incomingInstance.getKontaktinformasjon().getTelefonnummer()),
-                Map.entry("kontaktinformasjonEpostadresse", incomingInstance.getKontaktinformasjon().getEpostadresse()),
+        entries.add(Map.entry("personaliaFodselsnummer", incomingInstance.getPersonalia().getFodselsnummer()));
+        entries.add(Map.entry("personaliaFornavn", incomingInstance.getPersonalia().getFornavn()));
+        entries.add(Map.entry("personaliaMellomnavn", incomingInstance.getPersonalia().getMellomnavn()));
+        entries.add(Map.entry("personaliaEtternavn", incomingInstance.getPersonalia().getEtternavn()));
 
-                Map.entry("inntaksadresseGateadresse", incomingInstance.getInntaksadresse().getGateadresse()),
-                Map.entry("inntaksadressePostnummer", incomingInstance.getInntaksadresse().getPostnummer()),
-                Map.entry("inntaksadressePoststed", incomingInstance.getInntaksadresse().getPoststed()),
+        entries.add(Map.entry("kontaktinformasjonTelefonnummer", incomingInstance.getKontaktinformasjon().getTelefonnummer()));
+        entries.add(Map.entry("kontaktinformasjonEpostadresse", incomingInstance.getKontaktinformasjon().getEpostadresse()));
 
-                Map.entry("dokumentTittel", incomingInstance.getDokument().getTittel()),
-                Map.entry("dokumentDato", incomingInstance.getDokument().getDato()),
-                Map.entry("dokumentFilnavn", incomingInstance.getDokument().getFilnavn()),
-                Map.entry("dokumentFormat", incomingInstance.getDokument().getFormat()),
-                Map.entry("dokumentFil", uuid.toString()),
+        entries.add(Map.entry("inntaksadresseGateadresse", incomingInstance.getInntaksadresse().getGateadresse()));
+        entries.add(Map.entry("inntaksadressePostnummer", incomingInstance.getInntaksadresse().getPostnummer()));
+        entries.add(Map.entry("inntaksadressePoststed", incomingInstance.getInntaksadresse().getPoststed()));
 
-                Map.entry("tilleggsinformasjonSkolear", incomingInstance.getTilleggsinformasjon() != null ? incomingInstance.getTilleggsinformasjon().getSkolear() : ""),
-                Map.entry("tilleggsinformasjonSkolenummer", incomingInstance.getTilleggsinformasjon() != null ? incomingInstance.getTilleggsinformasjon().getSkolenummer() : ""),
-                Map.entry("tilleggsinformasjonSkolenavn", incomingInstance.getTilleggsinformasjon() != null ? incomingInstance.getTilleggsinformasjon().getSkolenavn() : ""),
-                Map.entry("tilleggsinformasjonProgramomradekode", incomingInstance.getTilleggsinformasjon() != null ? incomingInstance.getTilleggsinformasjon().getProgramomradekode() : ""),
-                Map.entry("tilleggsinformasjonProgramomradenavn", incomingInstance.getTilleggsinformasjon() != null ? incomingInstance.getTilleggsinformasjon().getProgramomradenavn() : ""),
-                Map.entry("tilleggsinformasjonSokertype", incomingInstance.getTilleggsinformasjon() != null ? incomingInstance.getTilleggsinformasjon().getSokertype() : ""))
+        entries.add(Map.entry("dokumentTittel", incomingInstance.getDokument().getTittel()));
+        entries.add(Map.entry("dokumentDato", incomingInstance.getDokument().getDato()));
+        entries.add(Map.entry("dokumentFilnavn", incomingInstance.getDokument().getFilnavn()));
+        entries.add(Map.entry("dokumentFormat", incomingInstance.getDokument().getFormat()));
+        entries.add(Map.entry("dokumentFil", uuid.toString()));
 
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        if (incomingInstance.getTilleggsinformasjon() != null) {
+            entries.add(Map.entry("tilleggsinformasjonSkolear", incomingInstance.getTilleggsinformasjon().getSkolear()));
+            entries.add(Map.entry("tilleggsinformasjonSkolenummer", incomingInstance.getTilleggsinformasjon().getSkolenummer()));
+            entries.add(Map.entry("tilleggsinformasjonSkolenavn", incomingInstance.getTilleggsinformasjon().getSkolenavn()));
+            entries.add(Map.entry("tilleggsinformasjonProgramomradekode", incomingInstance.getTilleggsinformasjon().getProgramomradekode()));
+            entries.add(Map.entry("tilleggsinformasjonProgramomradenavn", incomingInstance.getTilleggsinformasjon().getProgramomradenavn()));
+            entries.add(Map.entry("tilleggsinformasjonSokertype", incomingInstance.getTilleggsinformasjon().getSokertype()));
+        }
+
+        return entries.stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     private Mono<UUID> postFile(Long sourceApplicationId, IncomingInstance incomingInstance) {
