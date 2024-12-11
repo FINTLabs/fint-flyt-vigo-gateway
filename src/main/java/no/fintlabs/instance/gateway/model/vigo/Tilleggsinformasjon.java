895de49b8d379c080f66b54c597415f282a30303
Nils-Odd Solberg <nilsodd@fintlabs.no>
2024-12-11 14:37:59 +0100

    ARKIV-259 Tilleggsinformasjon for SOKNAD, SOKER_N, OPPMELDING, TILBUD, SVAR, FORMIDL, KONTRAKT, KONTRENDSOK, KONTRENDVED, OPPNEMD, PROTOKOLL, VEDLEGG_KON, VEDLEGG_PROV and FAGDOK 😅

--- src/main/java/no/fintlabs/instance/gateway/model/vigo/Tilleggsinformasjon.java
+++ src/main/java/no/fintlabs/instance/gateway/model/vigo/Tilleggsinformasjon.java
@@ -11,10 +11,40 @@ import lombok.extern.jackson.Jacksonized;
 @Builder
 public class Tilleggsinformasjon {
 
-    private String skolear;
-    private String skolenummer;
-    private String skolenavn;
+    private String endringstype;
+    private String fylkessignatar;
+    private String kandidattype;
+    private String kontraktsnummer;
+    private String kontraktspartEpost;
+    private String kontraktspartkode;
+    private String kontraktspartnavn;
+    private String kontraktspartnummer;
+    private String kontraktstype;
+    private String lastetOppAv;
+    private String lastetOppDato;
+    private String nemndnavn;
+    private String nemndnummer;
+    private String oppmeldtDato;
+    private String organisasjonsnavn;
+    private String organisasjonsnummer;
     private String programomradekode;
     private String programomradenavn;
+    private String provenr;
+    private String provestatus;
+    private String provetype;
+    private String resultatPraktisk;
+    private String resultatTeori;
+    private String skolear;
+    private String skolenavn;
+    private String skolenummer;
     private String sokertype;
+    private String vedleggBeskrivelse;
+    private String vedleggTittel;
+    private String vedtaksresultat;
+
+    // FAGDOK fields - may change in the future
+    private String fnr;
+    private String vgdoknr;
+    private String vgdoktype;
+    private String utsendtDato;
 }
